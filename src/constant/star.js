e0efeccdfb75a11000dc25b47c5c17366325ea92
cqchien <caochientp1@gmail.com>
2021-08-19 20:09:00 +0700

    create schema and model

---
+++ src/constant/star.js
@@ -0,0 +1,5 @@
+module.exports = {
+  min: 0,
+  default: 0,
+  max: 999,
+};
---
+++ src/models/word.model.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+const wordSchema = new Schema({
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  type: [
+    {
+      type: String,
+      require: true,
+      enum: ['n', 'adj', 'adv', 'v', 'con', 'pre', 'pro', 'det'],
+    },
+  ],
+  mean: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  example: {
+    type: String,
+    trim: true,
+  },
+  synonyms: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'word',
+    },
+  ],
+  antonyms: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'word',
+    },
+  ],
+  picture: {
+    type: String,
+  },
+  phonetic: {
+    type: String,
+  },
+  star: [{
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  }],
+});
+
+const wordModel = model('word', wordSchema, 'word');
+
+module.exports = wordModel;
