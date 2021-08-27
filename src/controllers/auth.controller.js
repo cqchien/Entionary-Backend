716de4b5f6c80ce5119a4bf0ca9631cf5636b490
cqchien <caochientp1@gmail.com>
2021-08-27 23:56:30 +0700

    api login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const httpStatus = require('http-status');
 const createUser = require('../services/user/create.service');
 const generateAuthToken = require('../services/token/generateAuth.service');
 const handleSuccess = require('../utils/successfulHandler');
+const loginWithEmail = require('../services/token/login.service');
 
 const register = async (req, res, next) => {
   const { body } = req;
@@ -15,6 +16,18 @@ const register = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const user = await loginWithEmail(body);
+    const token = await generateAuthToken(user);
+    return handleSuccess(res, { user, token }, httpStatus.OK);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
