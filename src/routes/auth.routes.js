5d700d2790b54e1a04788fdeda08b9d17f14c3a7
cqchien <caochientp1@gmail.com>
2021-08-27 23:57:14 +0700

    fix route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const { login } = require('../controllers/auth.controller');
-const register = require('../controllers/auth.controller');
+const { register } = require('../controllers/auth.controller');
 
 const router = Router();
 
 router.post('/register', register);
-router.post('login', login);
+router.post('/login', login);
 
 module.exports = router;
