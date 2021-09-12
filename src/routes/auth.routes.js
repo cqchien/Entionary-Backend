36c2f73c88ce8c0a14c761c494c62ecffeaf71c6
cqchien <caochientp1@gmail.com>
2021-09-12 15:50:05 +0700

    set validate

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const { login } = require('../controllers/auth.controller');
 const { register } = require('../controllers/auth.controller');
+const validate = require('../middlewares/validate.middleware');
+const { registerValidationSchema, loginValidationSchema } = require('../validations/auth.validation');
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validate(registerValidationSchema), register);
+router.post('/login', validate(loginValidationSchema), login);
 
 module.exports = router;
--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,14 +1,18 @@
 const Joi = require('joi');
 const { password } = require('./custom.validation');
 
-const register = {
-  body: Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().custom(password),
-  }),
-};
+const registerValidationSchema = Joi.object().keys({
+  name: Joi.string().required(),
+  email: Joi.string().required().email(),
+  password: Joi.string().required().custom(password),
+});
+
+const loginValidationSchema = Joi.object().keys({
+  email: Joi.string().required().email(),
+  password: Joi.string().required(),
+});
 
 module.exports = {
-  register,
+  registerValidationSchema,
+  loginValidationSchema,
 };
