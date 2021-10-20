f6f627d4923d1be687617d57ad95ed47b85ec197
cqchien <caochientp1@gmail.com>
2021-10-20 13:42:53 +0700

    api verifycode

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const httpStatus = require('http-status');
+const checkVerifyCode = require('../services/user/checkVerifyCode.service');
 const getUserByEmailOrId = require('../services/user/getOne.service');
+const updateUser = require('../services/user/update.service');
 const Exception = require('../utils/exception');
 const handleSuccess = require('../utils/successfulHandler');
 
@@ -16,6 +18,31 @@ const getUserDetail = async (req, res, next) => {
   }
 };
 
+const resetPassword = async (req, res, next) => {
+  const { email, codeToVerify, password } = req.body;
+  try {
+    // Check email
+    const userDetail = await getUserByEmailOrId({ email });
+    if (!userDetail) {
+      throw new Exception(httpStatus.NOT_FOUND, 'User Not Found');
+    }
+
+    // Validate code
+    const isMatchCode = checkVerifyCode(userDetail, codeToVerify);
+    if (!isMatchCode) {
+      throw new Exception(httpStatus.BAD_REQUEST, 'Code To Verify is invalid');
+    }
+
+    // Update password
+    await updateUser(userDetail._id, { password });
+
+    return handleSuccess(res, { ...userDetail, password }, httpStatus.OK);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getUserDetail,
+  resetPassword,
 };
