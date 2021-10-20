0721e994755d7672d7307405a9b466c638cef725
cqchien <caochientp1@gmail.com>
2021-10-20 13:43:11 +0700

    update route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const { getUserDetail } = require('../controllers/user.controller');
 const checkToken = require('../middlewares/checkToken.middleware');
+const validate = require('../middlewares/validate.middleware');
+const { resetPasswordValidationSchema } = require('../validations/user.validation');
 
 const router = Router();
 
 router.use(checkToken);
 
 router.get('/me', getUserDetail);
+router.patch('/reset-password', validate(resetPasswordValidationSchema));
 
 module.exports = router;
