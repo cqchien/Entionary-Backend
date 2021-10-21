3c7341bcee2a85e50684622fd71fca84f4d7ec6d
cqchien <caochientp1@gmail.com>
2021-10-21 23:46:22 +0700

    update docs

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -6,17 +6,31 @@ const swaggerDef = {
   info: {
     title: 'Entionary API documentation',
     version,
+    description:
+      'This is an API document for Events Marketing - Entionary projects. Read README.md to setup something before start.',
     license: {
       name: 'MIT',
+    },
+    contact: {
+      email: 'caochientp1@gmail.com',
     },
   },
   servers: [
     {
       url: `http://localhost:${config.port}`,
+      description: 'Local',
     },
     {
       url: `${config.server.URL_DEPLOY}`,
+      description: 'Online',
     },
+  ],
+  tags: [
+    {
+      name: 'auth',
+      description: 'Authentication for account.',
+    },
+    { name: 'user', description: 'Everything about user' },
   ],
 };
 
