06125962805df957f14c08a635ccf6a96e3766fc
cqchien <caochientp1@gmail.com>
2021-11-07 00:00:03 +0700

    update validate middleware

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,11 +1,14 @@
 const httpStatus = require('http-status');
 const Joi = require('joi');
 const Exception = require('../utils/exception');
+const pick = require('../utils/pick.js');
 
 const validate = (schema) => (req, res, next) => {
-  const { error } = Joi.compile(schema)
+  const validSchema = pick(schema, ['params', 'query', 'body']);
+  const object = pick(req, Object.keys(validSchema));
+  const { error } = Joi.compile(validSchema)
     .prefs({ errors: { label: 'key' } })
-    .validate(req.body);
+    .validate(object);
   // handle error
   if (error) {
     const errorMessages = error.details.map((errorDetail) => errorDetail.message).join('; ');
