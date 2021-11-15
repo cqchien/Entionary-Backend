e79216a41609722129ce4579f0b518b769ca9f58
cqchien <caochientp1@gmail.com>
2021-11-15 15:20:11 +0700

    add docs and fix update array in db

--- src/services/flashcard/updateOne.service.js
+++ src/services/flashcard/updateOne.service.js
@@ -1,7 +1,10 @@
 const flashcardModel = require('../../models/flashcard.model');
 
 const updateOneFlashcard = async (flashcardId, dataToUpdate) => {
-  const result = await flashcardModel.updateOne({ _id: flashcardId }, { ...dataToUpdate });
+  const result = await flashcardModel.updateOne(
+    { _id: flashcardId },
+    { ...dataToUpdate, $push: { words: dataToUpdate.word } },
+  );
   return result;
 };
 
