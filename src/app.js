56819a97e6e7a33d113915d937533b86799f91d7
cqchien <caochientp1@gmail.com>
2021-11-21 18:07:42 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -21,14 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // enable cors
-const corsOption = {
-  origin: 'http://47.254.250.188:5000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOption));
+app.use(cors());
 
 app.options('*', cors());
 
