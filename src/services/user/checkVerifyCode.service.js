8bf25637aeccb88d74d0145abbc4fb892e08bdbb
cqchien <caochientp1@gmail.com>
2021-11-22 22:06:48 +0700

    remove async when we not use

--- src/services/user/checkVerifyCode.service.js
+++ src/services/user/checkVerifyCode.service.js
@@ -1,8 +1,7 @@
 const verifyCodeConstant = require('../../constant/verifyCode');
 
-const checkVerifyCode = async (user, codeToVerify) => {
+const checkVerifyCode = (user, codeToVerify) => {
   const { code, updatedAt } = user.verifyCode;
-
   // Validate expire time of code to verify
   const expireTime = new Date().getTime() - new Date(updatedAt).getTime();
 
